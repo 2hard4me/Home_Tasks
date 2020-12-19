4a1e028855beb3b5e43f0f39bd4ed4833743f031
2hard4me <usmanov.aa@phystech.edu>
2020-12-19 14:33:17 +0300

    Fixed 6_2.c

--- 6_2.c
+++ 6_2.c
@@ -11,6 +11,60 @@
 #define handle_error(message) \
 	do { perror(message); exit(EXIT_FAILURE); } while (0);
 
+const char *PRINT_TYPE(mode_t file_mode) {
+	switch (file_mode & S_IFMT) {
+		case S_IFBLK:
+		      printf("Block device");
+		      break;
+		case S_IFCHR:
+		      printf("Character device");
+		      break;
+		case S_IFDIR:
+		      printf("Directory");
+		      break;
+		case S_IFIFO:
+		      printf("Fifo/pipe");
+		      break;
+		case S_IFLNK:
+		      printf("Symbolyc link");
+		      break;
+		case S_IFREG:
+		      printf("Regular file");
+		      break;
+		case S_IFSOCK:
+		      printf("Socket");
+		      break;
+		default:
+		      printf("Unknown?");
+		      break;
+	}
+}
+
+const char *PRINT_DIR_TYPE(__uint8_t type) {
+	switch (type) {
+		case DT_BLK: 
+			printf("Block device");
+			break;
+		case DT_DIR:
+			printf("Directory");
+			break;
+		case DT_CHR:
+			printf("Character device");
+			break;
+		case DT_FIFO:
+			printf("Fifo/pipe");
+			break;
+		case DT_LNK:
+			printf("Symbolic link");
+			break;
+		case DT_REG:
+			printf("Regular file");
+			break;
+		case DT_SOCK:
+			printf("Socket");	
+	}
+}
+
 
 int main(int argc, char *argv[]) {
 	if (argc > 2) {
@@ -50,57 +104,9 @@ int main(int argc, char *argv[]) {
 			if (fstatat(DirFileDesc, sd->d_name, &fileStat, 0) < 0) {
 				handle_error("Failed to stat\n");
 			}
-			switch (fileStat.st_mode & S_IFMT) {
-				case S_IFBLK:
-				      printf("Block device");
-				      break;
-				case S_IFCHR:
-				      printf("Character device");
-				      break;
-				case S_IFDIR:
-				      printf("Directory");
-				      break;
-				case S_IFIFO:
-				      printf("Fifo/pipe");
-				      break;
-				case S_IFLNK:
-				      printf("Symbolyc link");
-				      break;
-				case S_IFREG:
-				      printf("Regular file");
-				      break;
-				case S_IFSOCK:
-				      printf("Socket");
-				      break;
-				default:
-				      printf("Unknown?");
-				      break;
-			}
-
+			PRINT_TYPE(fileStat.st_mode);
 		} else {
-			switch (sd->d_type) {
-				case DT_BLK: 
-					printf("Block device");
-					break;
-				case DT_DIR:
-					printf("Directory");
-					break;
-				case DT_CHR:
-					printf("Character device");
-					break;
-				case DT_FIFO:
-					printf("Fifo/pipe");
-					break;
-				case DT_LNK:
-					printf("Symbolic link");
-					break;
-				case DT_REG:
-					printf("Regular file");
-					break;
-				case DT_SOCK:
-					printf("Socket");
-			}
-	
+			PRINT_DIR_TYPE(sd->d_type);
 		}
 
 		printf("Name: %s \n", sd->d_name);
