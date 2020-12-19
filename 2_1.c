4dc6548c2952dbfe311de22f31a053c3769bc705
2hard4me <usmanov.aa@phystech.edu>
2020-12-19 20:42:06 +0300

    Fixed 2_1.c

--- 2_1.c
+++ 2_1.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
-		printf("Usage: %s some.file string\n", argv[0]);
+		fprintf(stderr, "Usage: %s some.file string\n", argv[0]);
 		return 1;
 	}
 
